5bcb651886868777f45564d0246b107f3fe7886a
ivanhrt <ivanherreror@gmail.com>
2022-03-01 19:36:47 +0100

    auth correo, contraseña y logout funciona

--- GoogleCalendarMoodleKt/app/src/main/java/com/googlecalendarmoodlekt/AuthActivity.kt
+++ GoogleCalendarMoodleKt/app/src/main/java/com/googlecalendarmoodlekt/AuthActivity.kt
@@ -1,10 +1,11 @@
 package com.googlecalendarmoodlekt
 
-import res.*
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.FirebaseApp
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_auth.*
 
@@ -36,9 +37,63 @@ class AuthActivity : AppCompatActivity() {
     private fun setup() {
         title = "Autenticación"
 
-        /*signUpButton.setOnClickListener{
-            if (emailEditText.text.isNotEmpty())
-        }*/
+        logOutButton.setOnClickListener{
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                    emailEditText.text.toString(),
+                        passwordEditText.text.toString()).addOnCompleteListener {
+
+                            if (it.isSuccessful){
+                                showHome(it.result?.user?.email?:"",ProviderType.BASIC)
+                            } else{
+                                showAlert()
+
+                            }
+                    }
+            }
+        }
+
+        loginButton.setOnClickListener{
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(
+                        emailEditText.text.toString(),
+                        passwordEditText.text.toString()).addOnCompleteListener {
+
+                        if (it.isSuccessful){
+                            showHome(it.result?.user?.email?:"",ProviderType.BASIC)
+                        } else{
+                            showAlert()
+
+                        }
+                    }
+            }
+        }
+
+
+    }
+
+    private fun showAlert() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+
+        val homeIntent :Intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 
 
--- GoogleCalendarMoodleKt/app/src/main/java/com/googlecalendarmoodlekt/HomeActivity.kt
+++ GoogleCalendarMoodleKt/app/src/main/java/com/googlecalendarmoodlekt/HomeActivity.kt
@@ -2,11 +2,37 @@ package com.googlecalendarmoodlekt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType {
+    BASIC
+}
 
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Setup
+        val bundle :Bundle? = intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        setup(email ?:"", provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String) {
+
+        title = "Inicio"
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
     }
 }
