815cbbdd23092e289e96ce412271375f0171f9d2
ivanhrt <ivanherreror@gmail.com>
2022-03-13 21:53:24 +0100

    webview y opciones

--- GoogleCalendarMoodleKt/app/src/main/java/com/googlecalendarmoodlekt/HomeActivity.kt
+++ GoogleCalendarMoodleKt/app/src/main/java/com/googlecalendarmoodlekt/HomeActivity.kt
@@ -1,9 +1,14 @@
 package com.googlecalendarmoodlekt
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
@@ -15,6 +20,11 @@ enum class ProviderType {
 
 
 class HomeActivity : AppCompatActivity() {
+
+    //Private
+
+    private val URL_BASE = "https://calendar.google.com/calendar/u/0/r?pli=1"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -23,7 +33,7 @@ class HomeActivity : AppCompatActivity() {
         val bundle :Bundle? = intent.extras
         val email: String? = bundle?.getString("email")
         val provider: String? = bundle?.getString("provider")
-        setup(email ?:"", provider ?: "")
+        //setup(email ?:"", provider ?: "")
 
         //Guardado de datos
 
@@ -32,9 +42,56 @@ class HomeActivity : AppCompatActivity() {
         prefs.putString("provider",provider)
         prefs.apply()
 
+        //WebView
+
+        web_calendar.webChromeClient = object : WebChromeClient() {
+
+        }
+
+        web_calendar.webViewClient = object : WebViewClient() {
+
+        }
+
+        val settings = web_calendar.settings
+        settings.javaScriptEnabled = true
+
+        web_calendar.loadUrl(URL_BASE)
+
+
     }
 
-    private fun setup(email: String, provider: String) {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        title = getString(R.string.calendar)
+        when(item.itemId){
+            //Borrado de datos
+            R.id.action_logout ->{
+                val prefs: SharedPreferences.Editor = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+                prefs.clear()
+                prefs.apply()
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(this,AuthActivity::class.java))
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (web_calendar.canGoBack()){
+            web_calendar.goBack()
+        }
+        else{
+            finishAffinity()
+            finish()
+        }
+
+    }
+
+    /*private fun setup(email: String, provider: String) {
 
         title = "Inicio"
         emailTextView.text = email
@@ -52,5 +109,5 @@ class HomeActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-    }
+    }*/
 }
